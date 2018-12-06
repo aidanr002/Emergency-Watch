a488f84fbc648eb42228262e3fe045e325fa33fb
Aidan Ryan <aidanr002@gmail.com>
2018-12-06 15:59:49 +1000

    Added map activity & fragment

--- EmergencyWatch/app/src/main/java/com/cycloneproductions/aidan/emergencywatch/HomeActivity.java
+++ EmergencyWatch/app/src/main/java/com/cycloneproductions/aidan/emergencywatch/HomeActivity.java
@@ -1,36 +1,77 @@
 package com.cycloneproductions.aidan.emergencywatch;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: Oncreate is starting for HomeActivity");
+
+        if (isServicesOk()) {
+            Log.d(TAG, "onCreate: Services are ok, initialising map activity");
+            init();
+        }
     }
 
-    public void eventButtonClicked(View view) {
-        TextView fireHeading = (TextView) findViewById(R.id.fireHeading);
-        fireHeading.setText("Pressed");
+    private void init() {
+        Log.d(TAG, "init: Initialising onclicklistener for events map button from home activity");
+
+        Button buttontomap = (Button) findViewById(R.id.mapButton);
+        buttontomap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked button to map");
+
+                Intent intent = new Intent(HomeActivity.this, MapActivity.class);
+                startActivity(intent);
+                Log.d(TAG, "onClick: MapActivity should have/be starting");
+            }
+        });
     }
-    public void mapButtonClicked(View view) {
-        TextView mapButton = (TextView) findViewById(R.id.mapButton);
-        mapButton.setText("Pressed");
-        TextView listButton = (TextView) findViewById(R.id.listButton);
-        listButton.setText("List");
-    }
+
     public void menuButtonClicked(View view) {
         TextView menuButton = (TextView) findViewById(R.id.menuButton);
         menuButton.setText("Pressed");
     }
-    public void listButtonClicked(View view) {
-        TextView listButton = (TextView) findViewById(R.id.listButton);
-        listButton.setText("Pressed");
-        TextView mapButton = (TextView) findViewById(R.id.mapButton);
-        mapButton.setText("Map");
+
+    public boolean isServicesOk() {
+        Log.d(TAG, "isServicesOk: Checking Google Services Version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(HomeActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //Everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOk: Google play services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //An error occured but it can be resolved
+            Log.d(TAG, "isServicesOk: An error occured but it can be resolved");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(HomeActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
---
+++ EmergencyWatch/app/src/main/java/com/cycloneproductions/aidan/emergencywatch/MapActivity.java
@@ -0,0 +1,162 @@
+package com.cycloneproductions.aidan.emergencywatch;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapActivity";
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    // Variables
+    private Boolean mLocationPermissionGranted = false;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: Map is ready");
+        Toast.makeText(this, "Map Ready", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        Log.d(TAG, "onCreate: Oncreate is starting for MapActivity");
+
+        getLocationPermission();
+
+        Button buttontolist = (Button) findViewById(R.id.listButton);
+
+        buttontolist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked button to list");
+
+                Intent intent = new Intent(MapActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: Gettingt the devices current location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if(mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Found Location");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText((MapActivity.this), "Unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation: Security Exception:" + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: moving camera to: Lat:" + latLng.latitude + ", Lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void initMap() {
+        Log.d(TAG, "initMap: Initialising map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.eventMap);
+
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: Getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initMap();
+                Log.d(TAG, "getLocationPermission: Permissions Granted");
+            } else {
+                ActivityCompat.requestPermissions(this, permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: Called");
+        mLocationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: Permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: Permission Granted");
+                    mLocationPermissionGranted = true;
+                    //initialise the map
+                    initMap();
+                }
+            }
+        }
+    }
+}
+
